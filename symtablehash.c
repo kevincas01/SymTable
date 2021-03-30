ea5ec19488256b94e627ba0201fb08eddc32d870
Kevin Castro <kevvv@dynamic-oit-vapornet100-10-8-14-109.princeton.edu>
2021-03-30 01:19:16 -0400

    Update

--- symtablehash.c
+++ symtablehash.c
@@ -100,6 +100,8 @@ static SymTable_T SymTable_reposition(SymTable_T oSymTable) {
     {
        return oSymTable;
     }
+    newSymTable->bucketnum=oSymTable->bucketnum;
+    newSymTable->bindings=oSymTable->bindings;
     
     for (index = 0; index < oldsize; index++) {
         for ( currnode=oSymTable->hashbuckets[index]; currnode!=NULL; currnode=currnode->next){
